3b9a0f05f59ef46d26b57c2732f634030455c167
Hubert Vromman <hvromman@w-r11-p3.s19.be>
2019-05-11 08:45:48 +0200

    cool

--- server/main.go
+++ server/main.go
@@ -9,25 +9,36 @@ import (
 )
 
 type User struct {
-	username string
 	password string
 	nickname string
+	username string
 	cur_channel int
 }
 
-func sendData(conn net.Conn, message *string , users []User, user_id *int) {
+type Message struct {
+	data string
+	channel_id int
+}
+
+type Channel struct {
+	name string
+	users []User
+	is_open int
+}
+
+func sendData(conn net.Conn, message *Message, users *[]User, user_id *int, channels *[]Channel) {
 	for {
-		if *message != "" {
-			fmt.Println("New message :")
-			fmt.Print(*message)
-			conn.Write([]byte(*message))
+		if *user_id != -1 && message.channel_id != -1 {
+			if message.channel_id == (*users)[*user_id].cur_channel {
+				conn.Write([]byte(message.data))
+			}
 			<-time.After(time.Millisecond)
-			*message = ""
+			message.channel_id = -1
 		}
 	}
 }
 
-func getData(conn net.Conn, message *string, users *[]User, user_id *int) {
+func getData(conn net.Conn, message *Message, users *[]User, user_id *int, channels *[]Channel) {
 	reader := bufio.NewReader(conn)
 	for {
 		// buf := make([]byte, 0, 4096)
@@ -36,29 +47,39 @@ func getData(conn net.Conn, message *string, users *[]User, user_id *int) {
 			return
 		}
 		if strings.HasPrefix(buf, "PASS NICK USER") {
-			fmt.Printf("full %v\n", buf)
-			buf = strings.TrimSpace(strings.Replace(buf, "PASS NICK USER", "", 1))	
-			fmt.Printf("after replace %v\n", buf)
+			buf = strings.TrimSpace(strings.Replace(buf, "PASS NICK USER", "", 1))
 			separated := strings.Split(buf, " ")
-			fmt.Println(separated, len(separated))
 			*user_id = -1
 			for n := range *users {
-				if (*users)[n].username == separated[0] {
-					if (*users)[n].password == separated[1] {
+				if (*users)[n].username == separated[2] {
+					if (*users)[n].password == separated[0] {
 						*user_id = n
 					} else {
-						*message = "auth failed\n"
+						return //*message = "auth failed\n"
 					}
 				}
 			}
 			if *user_id == -1 {
-				*users = append(*users,  User{separated[0], separated[1], separated[0], -1})
+				*users = append(*users, User{separated[0], separated[1], separated[2], -1})
 				*user_id = len(*users) - 1;
 			}
 			fmt.Println((*users)[*user_id], *user_id)
-		} else {
-			newbuf := buf
-			*message = newbuf
+		} else if *user_id != -1 && strings.HasPrefix(buf, "JOIN") {
+			buf = strings.TrimSpace(strings.Replace(buf, "JOIN", "", 1))
+			(*users)[*user_id].cur_channel = -1
+			for n := range *channels {
+				if (*channels)[n].name == buf {
+					(*users)[*user_id].cur_channel = n
+					(*channels)[n].users = append((*channels)[n].users, (*users)[*user_id]);
+				}
+			}
+			if (*users)[*user_id].cur_channel == -1 {
+				*channels = append(*channels, Channel{buf, []User{(*users)[*user_id]}, 1})
+				(*users)[*user_id].cur_channel = len(*channels) - 1
+			}
+			fmt.Println((*channels)[(*users)[*user_id].cur_channel])
+		} else if *user_id != -1 && (*users)[*user_id].cur_channel != -1 {
+			*message = Message{buf, (*users)[*user_id].cur_channel}
 		}
 	}
 }
@@ -70,8 +91,9 @@ func main() {
 		// handle error
 	}
 	var users []User
+	var channels []Channel
 	var users_id []int
-	var message string
+	var message Message
 	// go getAllMessages(&str)
 	fmt.Println("Server started")
 	for {
@@ -81,7 +103,7 @@ func main() {
 		}
 		fmt.Println("New connection !")
 		users_id = append(users_id, -1)
-		go getData(conn, &message, &users, &users_id[len(users_id) - 1])
-		go sendData(conn, &message, users, &users_id[len(users_id) - 1])
+		go getData(conn, &message, &users, &users_id[len(users_id) - 1], &channels)
+		go sendData(conn, &message, &users, &users_id[len(users_id) - 1], &channels)
 	}
 }
