44fe213cdd0fda2c894c48e1d71076aea71e70d9
Hubert Vromman <hvromman@w-r11-p3.s19.be>
2019-05-12 15:20:33 +0200

    cool

--- server/command_func.go
+++ server/command_func.go
@@ -92,23 +92,20 @@ func	NAMES_cmd(msg string, id int, users *[]User)(int){
 	return (1)
 }
 
-func	LIST_cmd(msg []string, user_id int, users *[]User, channels *[]Channel)(int) {
+func	LIST_cmd(msg []string, user_id int, users *[]User, channels *map[string][]int)(int) {
 	fmt.Println("LIST_cmd:")
 	fmt.Println("Client: ", msg, len(msg))
 	send_mp((*users)[user_id], "321 :Channel Users Name")
 	if len(msg) > 1 {
 		splitted := strings.Split(msg[0], ",")
 		for _, chan_name := range splitted {
-			for _, channel := range *channels {
-				if chan_name == channel.name {
-					send_mp((*users)[user_id], "322 :" + chan_name + " " + strconv.Itoa(len(channel.users_id)) + " :[+nt]")
-					break
-				}
+			if users_id, ok := (*channels)[chan_name]; ok {
+				send_mp((*users)[user_id], "322 :" + chan_name + " " + strconv.Itoa(len(val)) + " :[+nt]")
 			}
 		}
 	} else {
-		for _, channel := range *channels {
-			send_mp((*users)[user_id], "322 :" + channel.name + " " + strconv.Itoa(len(channel.users_id)) + " :[+nt]")
+		for chan_name, users_id := range *channels {
+			send_mp((*users)[user_id], "322 :" + chan_name + " " + strconv.Itoa(len(users_id)) + " :[+nt]")
 		}
 	}
 	send_mp((*users)[user_id], "323 " + (*users)[user_id].nickname + " :End of /LIST")
